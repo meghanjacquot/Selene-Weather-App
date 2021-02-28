21fca709ef1e2b377e13cdf54a8f7361b1bb7277
Meghan Jacquot <meghanjacquot@gmail.com>
2021-02-28 11:51:40 -0500

    Test 2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
     let month = months[now.getMonth()];
     let daily = now.getDate();
     let year = now.getFullYear();
-    return `Today is ${day}, ${month} ${daily}, ${year}`;
+    return `This is a test ${day}, ${month} ${daily}, ${year}`;
 
 }
 //calling the date function
